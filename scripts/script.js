76b732a8f6c35e7d009a9f5570a07bf69da5ec57
Leonardo Sant'Anna <140214338+santannal@users.noreply.github.com>
2024-08-18 15:54:47 -0300

    finalizando

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+window.animacao = ScrollReveal({ reset: true });
+
+animacao.reveal('.carousel', {
+    duration: 300,
+    distance: '30px',
+})
+
+animacao.reveal('.container.marketing', {
+    duration: 300,
+    distance: '30px',
+})
+
+animacao.reveal('.container', {
+    duration: 300,
+    distance: '30px',
+})
+
+animacao.reveal('.fs-4', {
+    duration: 300,
+    distance: '30px',
+})
+
+animacao.reveal('.title-content ', {
+    duration: 300,
+    distance: '30px',
+})
